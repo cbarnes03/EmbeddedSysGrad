e74a9ac019636061daf82ac791cffcf67eea98d0
Charles Barnes <onthisrock97@gmail.com>
2021-09-28 20:01:19 -0400

    I commented in the specific hex values required to output several other colors.

--- CforLab1.c
+++ CforLab1.c
@@ -13,8 +13,17 @@ SysCtlClockSet(SYSCTL_SYSDIV_5|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN)
 	GPIOPinTypeGPIOOutput(GPIO_PORTF_BASE, GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3); // Declares that Pins 1, 2, 3 are Outputs
 while(1){	//Beginning of infinite WHILE loop
 	GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x08); // 0x08 = 1000 in binary, this enables the 4th bit(pin 3) and turns on the LED
+	// Color		LED(s)		PortF
+	// dark			---			0
+	// red			R--			0x02
+	// blue			--B			0x04
+	// green		-G-			0x08
+	// yellow		RG-			0x0A
+	// sky blue		-GB			0x0C
+	// white		RGB			0x0E
 	SysCtlDelay(20000000); // Adds 20,000,000 cycle delay = 1.25seconds
 	GPIOPinWrite(GPIO_PORTF_BASE,GPIO_PIN_1|GPIO_PIN_2|GPIO_PIN_3, 0x00); // 0x00 = 0000 in binary, this disables the 4th bit(pin 3) and turns off the LED
 	SysCtlDelay(20000000); // Adds 20,000,000 cycle delay = 1.25seconds
+	
 }
 }
