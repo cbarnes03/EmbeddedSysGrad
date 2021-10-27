ce02be5a5c8b8ba01049277dd4b1594a6c424fbf
Charles Barnes <onthisrock97@gmail.com>
2021-10-26 20:16:25 -0400

    The code was altered to ensure sysctl operation at 50MHz and commented to further explain total functionality. Something to note is that the while loop is intentionally left empty to serve as an endless loop in which to trap the execution when we wish to suspend calculations.

--- FPUwTivaWareLab4.c
+++ FPUwTivaWareLab4.c
@@ -12,17 +12,17 @@ float gSeriesData[SERIES_LENGTH];
 int dataCount = 0;
 int main(void)
 {
-float fRadians;
-ROM_FPULazyStackingEnable();
-ROM_FPUEnable();
-ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_16MHZ|SYSCTL_OSC_MAIN);
-fRadians = ((2 * M_PI) / SERIES_LENGTH);
+float fRadians;											// this variable is needed to calculate sine
+ROM_FPULazyStackingEnable();							
+ROM_FPUEnable();										// turns on the FPU
+ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_50MHZ|SYSCTL_OSC_MAIN);		// Sets up clock for 50MHz
+fRadians = ((2 * M_PI) / SERIES_LENGTH);												
 while(dataCount < SERIES_LENGTH)
 {
-gSeriesData[dataCount] = sinf(fRadians * dataCount);
+gSeriesData[dataCount] = sinf(fRadians * dataCount);		// Calculates the full cycle of a sine wave in side a 100-data point long array
 dataCount++;
 }
-while(1)
+while(1)													// the Suspend button traps the execution inside this endless While Loop
 {
 }
 } 
