ff16076f495e65ae40bcdab67ea3bcd68d37a6d1
Charles Barnes <onthisrock97@gmail.com>
2021-11-09 19:36:57 -0500

    This code now contains description of its function.

--- FPUwTivaWareLab4.c
+++ FPUwTivaWareLab4.c
@@ -1,20 +1,20 @@
-#include <math.h>
+#include <math.h>										// The code uses sinf() function which is prototyped by this header file 
 #include "inc/hw_memmap.h"
 #include "inc/hw_types.h"
-#include "driverlib/fpu.h"
+#include "driverlib/fpu.h"								// This header file provides support for Floating Point Unit
 #include "driverlib/sysctl.h"
 #include "driverlib/rom.h"
-#ifndef M_PI
+#ifndef M_PI											// In the case of M_PI not being defined, ifndef defines it
 #define M_PI 3.14159265358979323846
 #endif
-#define SERIES_LENGTH 100
-float gSeriesData[SERIES_LENGTH];
-int dataCount = 0;
+#define SERIES_LENGTH 100								// SERIES_LENGTH is the depth of our data buffer
+float gSeriesData[SERIES_LENGTH];						// This line is an array of floats SERIES_LENGTH long
+int dataCount = 0;										// This is a counter for our computation loop
 int main(void)
 {
-float fRadians;											// this variable is needed to calculate sine
+float fRadians;											// This variable is needed to calculate sine
 ROM_FPULazyStackingEnable();							
-ROM_FPUEnable();										// turns on the FPU
+ROM_FPUEnable();										// Turns on the FPU
 ROM_SysCtlClockSet(SYSCTL_SYSDIV_4|SYSCTL_USE_PLL|SYSCTL_XTAL_50MHZ|SYSCTL_OSC_MAIN);		// Sets up clock for 50MHz
 fRadians = ((2 * M_PI) / SERIES_LENGTH);												
 while(dataCount < SERIES_LENGTH)
@@ -22,7 +22,7 @@ while(dataCount < SERIES_LENGTH)
 gSeriesData[dataCount] = sinf(fRadians * dataCount);		// Calculates the full cycle of a sine wave in side a 100-data point long array
 dataCount++;
 }
-while(1)													// the Suspend button traps the execution inside this endless While Loop
+while(1)													// The Suspend button traps the execution inside this endless While Loop
 {
 }
 } 
